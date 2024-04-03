551407715a23af2745998f5246b178eaa13fafc6
GlitchedLime <106375444+Gl1tch3dL1m3@users.noreply.github.com>
2024-04-03 03:55:08 +0200

    Create participants.js

---
+++ participants.js
@@ -0,0 +1,21 @@
+$eval
+var title = ""
+var desc = ""
+
+try {
+  var list = "```"
+  if (users$channelID.length != 0) {
+    for (let i = 0 ; i < users$channelID.length ; i++) {list += "\n@" + users$channelID[i]}
+
+  } else {list += "nobody"}
+
+  list += "```"
+
+  title = "Participants of the giveaway 📊"
+  desc = "There are **" + users$channelID.length + "** participants of the giveaway. And here is a list of them:\n" + list
+ 
+} catch(e) {
+  title = "Error!"
+  desc = "There isn't any giveaway to fetch a list of participants from in this channel. ❌"
+}
+$halt $get(title)
