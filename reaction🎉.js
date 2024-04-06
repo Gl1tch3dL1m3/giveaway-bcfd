ba461f64c6b4b8d255d52a36b684a009f6ccd2e1
GlitchedLime <106375444+Gl1tch3dL1m3@users.noreply.github.com>
2024-04-06 17:14:29 +0200

    Update reaction🎉.js

--- reaction🎉.js
+++ reaction🎉.js
@@ -1,13 +1,27 @@
 $eval
-// This script won't send any message, but it works
+// Add "$get(desc)" into "description" field
+
+var title = ""
+var desc = ""
 
 try {
-  if (joined$channelID.indexOf($ID) == -1) {
+  var joinedindex = joined$channelID.indexOf($ID)
+
+  if (joinedindex == -1) {
     joined$channelID.push($ID)
     users$channelID.push("$namePlain")
+    title = "You have joined the giveaway! 🎉"
+    desc = "You have successfully joined the giveaway. If you want to leave, react with 🎉 again."
+
+  } else {
+    joined$channelID.splice(joinedindex, 1)
+    users$channelID.splice(joinedindex, 1)
+    title = "You have left the giveaway! ❌"
+    desc = "You have successfully left the giveaway. If you want to join again, react with 🎉 again."
   }
   
 } catch(e) {
   // Leave this blank...
+  title = e
 }
-$halt
+$halt $get(title)
